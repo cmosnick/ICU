e2d8a39f562002268112e73e0f9b258a4a7a62d7
Christina Mosnick <cmosnick07@gmail.com>
2016-10-09 12:08:05 -0500

    Got api to run on vagrant up

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
+# Install and start apache
+sudo apt-get update
+sudo apt-get install -y apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
 apache2ctl start
+
+# Install flask api
+printf "Y" | sudo apt-get install python-pip
+cd /vagrant/api
+pip install Flask-API
+chmod a+x app.py
+python app.py &
+
